7df76e8c8de2ce204733737483b4711e933f5f5e
jbki <jbki@protonmail.com>
2016-06-15 09:07:51 +0000

    initial commit

---
+++ src/AymardBundle/AymardBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AymardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AymardBundle extends Bundle
+{
+}
---
+++ src/AymardBundle/Resources/public/js/basic.js
@@ -0,0 +1,3 @@
+angular.module('myApp', []).config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
