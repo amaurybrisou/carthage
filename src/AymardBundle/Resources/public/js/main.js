b0bb521ca82617cd0ae67a0239cedc8d47bd8b76
jbki <jbki@protonmail.com>
2016-06-18 01:31:10 +0000

    side menu animations done in javascript, open by default

--- src/AymardBundle/Resources/public/js/main.js
+++ src/AymardBundle/Resources/public/js/main.js
@@ -4,16 +4,32 @@ angular.module('aymardApp', []).config(function($interpolateProvider){
 
 $(document).ready(function(){
 
-    $("#side-menu")
-        .mouseover(function(){
-            window.setTimeout(function(){
-                $("#page-description").removeClass("hidden");
-            }, 0)
-        })
-        .mouseleave(function(){
-            $("#page-description").addClass("hidden");
-        })
-    $('#menu-trigger').click(function(){
-        $("#side-menu").toggleClass("menu-active");
+    // $("#side-menu")
+    //     .mouseover(function(){
+    //         window.setTimeout(function(){
+    //             $("#page-description").removeClass("hidden");
+    //         }, 0)
+    //     })
+    //     .mouseleave(function(){
+    //         $("#page-description").addClass("hidden");
+    //     })
+        
+    $('.menu-trigger').click(function(){
+        if($(this).hasClass("active"))
+        {
+            $(this).removeClass("active");
+            $("#side-menu").stop().animate({width:'0vw'}, {queue: false, duration:300})
+            $(this).stop().animate({left:'0vw'}, {queue: false, duration: 300})
+        }
+        else
+        {
+            $(this).addClass("active");
+            $("#side-menu").stop().animate({width:'40vw'}, {queue: false, duration:300})
+            $(this).stop().animate({left:'32vw'}, {queue: false, duration: 300})
+
+        }
+
     })
+    
+ 
 })
